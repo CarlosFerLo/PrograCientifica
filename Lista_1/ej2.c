49b3790d645d31d811f4da1e7b38a4b4b9cb3c2b
CarlosFerLo <carlos.ferlo@outlook.com>
2023-02-16 19:27:53 +0100

    Ejercicio 2 - Funciona

--- Lista_1/ej2.c
+++ Lista_1/ej2.c
@@ -1,33 +1,42 @@
 #include <stdio.h>
-#include <math.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
 
-int pertenece(double x, double y, double z) ;
+#define L 3
+
+int cont (double x, double y, double z) ;
+void randxyz(double * x, double * y, double * z) ;
 
 int main (void) {
-	int n, i, cont ;
+	int n, k, rep, i, j ;
 	double x, y, z ;
-	srand(time(NULL)) ;
+	srand(time(NULL)) ;	
 
-	scanf(" %d", &n) ;
-	
-	for (i = 0, cont = 0; i < n; i++) {
-		x = rand() / RAND_MAX ;
-		y = rand() / RAND_MAX ;
-		z = rand() / RAND_MAX ;
-
-		if (pertenece(x, y, z) == 1) cont++ ;
+	printf("Introduce n, rep: ");
+	scanf(" %d %d", &n, &rep) ;
+	for (j = 0; j < rep; j++) {
+		for (i = 0, k = 0; i < n; i++) {
+			randxyz(&x, &y, &z) ;
+			k += cont(x, y, z) ;
+		}
+		printf("k = %d V = %lf\n", k, L*L*L*( (double) k/n )) ;
 	}
-
-	printf("Volumen: %lf\n", (1. * cont) / n);
+	
 
 	return 0 ;
 }
 
-int pertenece(double x, double y, double z) {
-	if (x*x + sin(y) <= z && x - z + exp(y) <= 1)
-		return 1;
-	else
-		return 0; 
+int cont (double x, double y, double z) {
+	if (0 <= x && x <= 1 && y >= 0 && y <= 1 && z >= 0 && z <= 1) {
+		if (x * x + sin(y) <= z && x - z + exp(y) <= 1) return 1;
+	}
+	return 0 ;
+}
+
+void randxyz (double * x, double * y, double * z) {
+	*x = (double) rand() / RAND_MAX * L ;
+	*y = (double) rand() / RAND_MAX * L ;
+	*z = (double) rand() / RAND_MAX * L ;
+	return ;
 }
