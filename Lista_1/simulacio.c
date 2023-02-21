befd4db77900374c72ed8bef7edc4461ddc10a8b
CarlosFerLo <carlos.ferlo@outlook.com>
2023-02-21 17:17:22 +0100

    Resolver error en simulacion.c

--- Lista_1/simulacio.c
+++ Lista_1/simulacio.c
@@ -16,7 +16,7 @@ int main (void) {
 
 	for (i = 0, cont = 0; i < M; i++) {
 		for (j = 0; j < n; j++) {
-			hist[j] = rand() % 364 + 1 ;
+			hist[j] = rand() % 365 + 1 ;
 			if (includes(hist[j], j, hist)) {
 				cont += 1 ;
 				break ;
